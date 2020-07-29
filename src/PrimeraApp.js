ea8c0e782d84dded96f259c2f16ca0b864b8ecb9
David Castello <davidcastellobatetgmail.com>
2020-07-29 14:32:50 +0200

    PrimeraApp

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,7 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types";
+//import React, { Fragment } from "react";
 
-const PrimeraApp = () => {
-  return <h1>Hola Mundo</h1>;
+const PrimeraApp = ({ saludo, edad, subtitulo }) => {
+  return (
+    <>
+      <h1>
+        {saludo} y tengo {edad} años.
+      </h1>
+      <p>{subtitulo}</p>
+    </>
+  );
 };
+
+//Definimos que tipo de props hay que pasar y si es obligatorio
+
+PrimeraApp.propTypes = {
+  saludo: PropTypes.string.isRequired,
+  edad: PropTypes.number,
+};
+
+PrimeraApp.defaultProps = {
+  subtitulo: "Subtitulo por defecto",
+};
+
+//Definir propiedades por defecto
 
 export default PrimeraApp;

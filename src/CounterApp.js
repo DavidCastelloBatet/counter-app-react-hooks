fe99574dba4913174226b02c9f992b17019e6410
David Castello <davidcastellobatetgmail.com>
2020-07-29 16:18:41 +0200

    Logica acabada

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(value);
+
   const handleAdd = () => {
-    value = value + 1;
-    console.log("Hola Mundo");
-    console.log(value);
+    setCounter(counter + 1);
+    //setCounter((counter) => counter +1);
+  };
+
+  const handleReset = () => {
+    setCounter(value);
+  };
+
+  const handleRest = () => {
+    setCounter(counter - 1);
   };
 
   return (
@@ -14,11 +23,11 @@ const CounterApp = ({ value }) => {
       <h2>
         with <span className="usestate">useState hook</span>
       </h2>
-      <h2>Initial value: {value}</h2>
+      <h2>Initial value: {counter}</h2>
       <div className="cuadro">
         <button onClick={handleAdd}>+1</button>
-        <button>Resset</button>
-        <button>-1</button>
+        <button onClick={handleReset}>Resset</button>
+        <button onClick={handleRest}>-1</button>
       </div>
     </>
   );

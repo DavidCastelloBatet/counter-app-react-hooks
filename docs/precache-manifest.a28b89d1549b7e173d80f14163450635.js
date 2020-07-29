ec1c2701a9b79c464e00e5bcce760639520b1862
David Castello <davidcastellobatetgmail.com>
2020-07-29 17:24:20 +0200

    version para build

---
+++ docs/precache-manifest.a28b89d1549b7e173d80f14163450635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bbc14fa605099caffca973de1da7463",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e060d408615487cc1757",
+    "url": "/static/css/main.93b99952.chunk.css"
+  },
+  {
+    "revision": "a31608a0db17a054242d",
+    "url": "/static/js/2.9f0dacec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f0dacec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e060d408615487cc1757",
+    "url": "/static/js/main.65b13490.chunk.js"
+  },
+  {
+    "revision": "79aa3d39d4c1bd445b47",
+    "url": "/static/js/runtime-main.753b9ab4.js"
+  }
+]);
